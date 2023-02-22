9d216ae1e9ea663a98e4337953cc8edfb958118d
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-02-21 23:37:56 -0500

    Beginning of Specials section

--- capstone-app/src/Components/Main.js
+++ capstone-app/src/Components/Main.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import HeroImage from "./Images/restaurantfood.jpg";
+import bruschetta from "./Images/bruchetta.svg";
+import salad from "./Images/greek salad.jpg";
+import cake from "./Images/lemon dessert.jpg";
+import person1 from "./Images/Person 1.jpg";
+import person2 from "./Images/Person 2.jpg";
+import person3 from "./Images/Person 3.jpg";
+import person4 from "./Images/Person 4.jpg";
+import mario1 from "./Images/Mario and Adrian A.jpg";
+import mario2 from "./Images/Mario and Adrian B.jpg";
+import dish from "./Images/Dish icon.svg";
+import cc from "./Images/creditcard.svg";
+import star from "./Images/Star Icon.jpg";
+
+
+
 function Main() {
     return (
         <>
@@ -22,13 +37,37 @@ function Main() {
         </header>
         <main className='main'>
             <section className='specials'>
+                <h1 style={{color:"#333333"}}>This Week's Specials!</h1>
+                <button className='button'>Online Menu</button>
+                <div className='card' id='salad'>
+                <img src={salad} alt="Fresh greek salad" />
+                <h3>Greek Salad</h3><h5 id='price'>$12.99</h5>
+                <p style={{color:"#333333"}}>The famous Greek Salad: crispy lettuce, peppers, olives, and our Chicago style feta cheese; garnished with chunky garlic and rosemary croutons</p>
+                <p style={{color:"#333333"}}>Order Now</p><img src={dish} alt="Dish icon" id='dish'/>
+                </div>
+                <div className='card'>
+                <img src={bruschetta} alt="Fresh and delicious bruschetta" id='brus'/>
+                <h3>Bruschetta</h3><h5 id='price'>$14.99</h5>
+                <p style={{color:"#333333"}}>Our bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+                <p style={{color:"#333333"}}>Order Now</p><img src={dish} alt="Dish icon" id='dish'/>
+                </div>
+                <div className='card'>
+                <img src={cake} alt="Sweet and delectable lemon cake" />
+                <h3>Lemon Cake</h3><h5 id='price'>$9.99</h5>
+                <p style={{color:"#333333"}}>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                <p style={{color:"#333333"}}>Order Now</p><img src={dish} alt="Dish icon" id='dish'/>
+                </div>
 
             </section>
             <section className='testimonials'>
-
+                <img src={person1} alt='Reviewer' />
+                <img src={person2} alt='Reviewer' />
+                <img src={person3} alt='Reviewer' />
+                <img src={person4} alt='Reviewer' />
             </section>
             <section className='about'>
-
+                <img src={mario1} alt="Head Chefs Mario and Adrian" />
+                <img src={mario2} alt="Head Chefs Mario and Adrian" />
             </section>
         </main>
         </>
