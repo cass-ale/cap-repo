eb632a2303c0d23d974c3eadded655b5a5a5687d
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-02-23 19:40:17 -0500

    Responsive CSS

--- capstone-app/src/Components/Main.js
+++ capstone-app/src/Components/Main.js
@@ -91,8 +91,8 @@ function Main() {
                     <p>Founded in 2013, Little Lemon's cuisine aims to capture the simplicity and joy of Mediterranean dining. Executive Chefs Mario Lennox and Adrian Moore were both born and raised in Chicago, and their stellar attention to detail ensures that every visit feels as magical as the first. Here at Little Lemon, you'll be welcomed in as family. We know you'll enjoy your meal at your soon-to-be favorite restaurant!</p>
                 </div>
                 <div className='abtimg'>
-                <img src={mario1} alt="Head Chefs Mario and Adrian" />
-                <img src={mario2} alt="Head Chefs Mario and Adrian" />
+                <img src={mario1} alt="Head Chefs Mario and Adrian" id="abtimg" />
+                <img src={mario2} alt="Head Chefs Mario and Adrian" id='abtimg'/>
                 </div>
             </section>
         </main>
